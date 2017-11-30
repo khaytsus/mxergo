42bc2dfd61c76210e2fba7c1b449283c87adc968
Walter Francis <wally@theblackmoor.net>
2017-11-30 14:34:37 -0500

    Rather than ID's, specify names of devices in mousespeed.sh
    Should work across reboots, systems, etc now.

--- mousespeed.sh
+++ mousespeed.sh
@@ -6,8 +6,8 @@ DISPLAY=:0
 xbindkeys
 
 # USB Unified Receiver for MX Ergo
-xinput --set-prop 17 'libinput Accel Speed' -.3
-xinput --set-prop 17 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 .6
+xinput --set-prop "pointer:Logitech MX Ergo" 'libinput Accel Speed' -.3
+xinput --set-prop "pointer:Logitech MX Ergo" 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 .6
 
 # USB Trackman Marble
 xinput --set-prop 10 'libinput Accel Speed' 0
